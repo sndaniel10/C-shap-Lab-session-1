6c99ed17fdffd40e451f0a4b37434c6ef6c67dc5
sndaniel10 <snathandaniel81@gmail.com>
2024-01-24 14:43:45 +0530

    Add project files.

---
+++ p1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter lenght");
+            double lenght= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter width");
+            double width= double.Parse(Console.ReadLine());
+
+            double area = Area(lenght, width);
+
+            Console.WriteLine("the area is" + area);
+            Console.ReadLine();
+
+
+        }
+
+        static double Area(double lenght , double width)
+        {
+            return width * lenght;
+        }
+    }
+}
---
+++ p2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write("\n enter number :");
+                num= int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    Console.Write("its even");
+                }
+                else
+                {
+                    Console.Write("its odd");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
