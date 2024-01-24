483d7cd58d50c60f2e2282950db1e62a90973eaf
sndaniel10 <135859295+sndaniel10@users.noreply.github.com>
2024-01-24 15:01:15 +0530

    Add files via upload

---
+++ p3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num , sum = 0;
+            Console.WriteLine("enter pos number");
+            num = int.Parse(Console.ReadLine());
+            
+            if (num > 0)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    sum = sum + i;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("error");
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
